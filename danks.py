3b1e6a787074b201540c7845678d9b275326aeae
DankBroom <91276577+DankBroom@users.noreply.github.com>
2022-09-27 08:57:41 +0200

    Adding plugins

--- danks.py
+++ danks.py
@@ -3,3 +3,16 @@
 # created the 20/09/2022
 # plugin of : FurastaOS, main.py
 # -------------------------------------------------------
+
+
+testPlugValue = False
+
+class plugin():
+    def plug(plugin):
+        if plugin == "testPlug.frp" and not testPlugValue:
+            testPlugValue = True
+            return "Pluged !"
+        elif plugin == "testPlug.frp" and testPlugValue:
+            return "Already pluged !"
+        else:
+            return "ERROR"
